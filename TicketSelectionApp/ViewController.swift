9dca93d42ec990b5c5b44d864fdda1691e54afc6
Eduard Laryushkin <info@edlar.ru>
2019-07-29 14:59:53 +0300

    Initial Commit

---
+++ TicketSelectionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicketSelectionApp
+//
+//  Created by Эдуард Ларюшкин on 29/07/2019.
+//  Copyright © 2019 edlar.ru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
